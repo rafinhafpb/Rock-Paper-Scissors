15c970ce8de178c4e02acb64b62b4b46175fcb9b
Rafael Benzi <74828555+rafinhafpb@users.noreply.github.com>
2024-12-01 16:59:57 +0100

    Update arm_interface.py

--- arm_interface.py
+++ arm_interface.py
@@ -97,7 +97,7 @@ class M3Proc:
 
       except Exception,e:
           print e
-      #self.proxy.step()
+          self.proxy.step()
 
   def step_openHand(self):
 #        self.bot.set_mode_off(self.hand_name)q
@@ -185,31 +185,39 @@ if __name__ == '__main__':
   t=M3Proc()
   try:
 
+    #   hand_set = [0, 250, 250, 250, 250]
+    #   arm_set = [0,-10, 20,100,-90,0, -30]
+
       hand_set = [0, 250, 250, 250, 250]
       arm_set = [0,0, 0,0,0,0, 0]
-
       host = 'localhost'
       port = 65432
       socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       socket.bind((host, port))
-      socket.listen(1)
-
-      connection, address = socket.accept()
 
       t.start()
-      while(1):
+      arm_set = [0, -10, 20, 100, -90, 0, -30]
+  
+      t.step_setHand(rock)
+      t.step_setArm(arm_set)
+      t.proxy.step()
 
+      while(1):
+        
+        socket.listen(1)
+        connection, address = socket.accept()
         try:
             data = connection.recv(1024)
             if not data:
                 break
-            command = float(data.decode())
-
+            command = int(data.decode())
             if command is not None:
+               print(command)
                if(command == 0):
                   t.bouncing()
                else:
                   t.doAction(command)
+                  time.sleep(3)
 
         except Exception as e:
             #print exception info
@@ -219,10 +227,9 @@ if __name__ == '__main__':
             break
 
   except (KeyboardInterrupt,EOFError):
-      hand_set = [0, 0, 0, 0, 0]
       arm_set = [0,0, 0,0,0,0, 0]
 
-      t.step_setHand(hand_set)
+      t.step_setHand(paper)
       t.step_setArm(arm_set)
       t.proxy.step()
       proxy.stop()
@@ -230,163 +237,3 @@ if __name__ == '__main__':
       time.sleep(1)
 
   t.stop()
-
-
-
-# #!/usr/bin/env python
-
-# import time
-# import m3.toolbox as m3t
-# import m3.rt_proxy as m3p
-# import m3.humanoid 
-# import m3.hand
-# import m3.gui as m3g
-# import m3.trajectory as m3jt
-# import numpy as nu
-# import yaml
-# from threading import Thread 
-# import sys
-# import time
-
-# class M3Proc:
-#     def __init__(self):
-#         self.proxy = m3p.M3RtProxy()
-
-#     def stop(self):
-#         self.proxy.stop()
-
-#     def start(self):
-#         # ######## Setup Proxy and Components #########################
-#         self.proxy.start()
-#         self.current_first = True
-
-       
-#         bot_name=m3t.get_robot_name()
-#         if bot_name == "":
-#             print 'Error: no robot components found:', bot_names
-#             return
-#         self.bot=m3.humanoid.M3Humanoid(bot_name)    
-#         arm_names = self.bot.get_available_chains()    
-#         arm_names = [x for x in arm_names if x.find('arm')!=-1]
-#         if len(arm_names)==0:
-#             print 'No arms found'
-#             return
-       
-#         ##PH
-#         self.arm_name='left_arm'
-#         ##PH
-
-#         # ####### Setup Hand #############
-#         self.hand_name='left_hand'
-#         # ####### Setup Proxy #############
-#         self.proxy.subscribe_status(self.bot)
-#         self.proxy.publish_command(self.bot)
-#         self.proxy.make_operational_all()
-#         self.bot.set_motor_power_on()
-#         self.ndof=self.bot.get_num_dof(self.arm_name)
-       
-#         humanoid_shm_names=self.proxy.get_available_components('m3humanoid_shm')
-#         print("gmwkrmlkhnwrllwrnrww")
-#         print(humanoid_shm_names)
-#         if len(humanoid_shm_names) > 0:
-           
-#             try:
-#                 self.proxy.make_safe_operational(humanoid_shm_names[0])
-#                 print(" inciou bbbbbbbbbbbbbbbbbbbbbbbbb")
-#             except:
-#                 print("nao inciou aaaaaaaaaaaaaaaaaaaa")
-
-#                                                                                                 # [angulacao altura braco pelo ombro, abertura braco, giro do braco -26.0, 8.0, cotovelo, angulo mao, -36.0, -2.0]
-#         #self.poses={'holdup':{'right_arm':[15.0, 26.0, -8.0, 84.0, 119.0, -36.0, 2.0],'left_arm':[56.0, -26.0, 8.0, 84.0, -119.0, -36.0, -2.0]}} 
-#         self.poses={'holdup':{'right_arm':[15.0, 26.0, -8.0, 84.0, 119.0, -36.0, 2.0],'left_arm':[20, 0.0, 0.0, 100.0, -120.0, -36.0, -2.0]}} 
-#         self.poses['holdup1']={'right_arm':[15.0, 26.0, -8.0, 84.0, 119.0, -36.0, 2.0],'left_arm':[50, 0.0, 0.0, 50.0, -120.0, -36.0, -2.0]}
-
-#         self.stiffness=60
-#         self.velocity=[0]
-#         self.theta_curr = [0.0]*self.ndof
-#         self.off=False
-#         self.openHand=False
-#         self.handCloseLevel=100.0
-#         self.handTorqCoeff=nu.array([1.0,1.0,0.9,1.0])
-#         self.handStiffness=80.0
-
-#     def step(self):
-#         try:
-#             self.step_hold_up()
-        
-
-
-
-#             self.proxy.step()
-
-#         except Exception,e:
-#             print e
-#         #self.proxy.step()
-
-#     def step_openHand(self):
-# #        self.bot.set_mode_off(self.hand_name)q
-#         self.bot.set_stiffness(self.hand_name,[self.handStiffness/100.0]*3,[2,3,4])
-#         self.bot.set_slew_rate_proportion(self.hand_name,[10.0]*3,[2,3,4])
-#         self.bot.set_mode_torque_gc(self.hand_name,[2,3,4])
-#         self.bot.set_torque(self.hand_name,self.handTorqCoeff[1:]*350,[2,3,4])
-#         self.bot.set_mode_theta_gc(self.hand_name,[0,1,2,3,4])
-#         self.bot.set_theta_deg(self.hand_name,[00,00,00,00,00.0],[0,1,2,3,4])
-#         #self.bot.set_theta_deg(self.hand_name,[300,300,300,300,300.0],[0,1,2,3,4])
-
-#     def step_closeHand(self):
-#         self.bot.set_stiffness(self.hand_name,[self.handStiffness/100.0]*3,[2,3,4])
-#         self.bot.set_slew_rate_proportion(self.hand_name,[1.0]*3,[2,3,4])
-
-# #        self.bot.set_mode_theta(self.hand_name,[0])
-# #        self.bot.set_mode_torque_gc(self.hand_name,[2,3,4])
-# #        self.bot.set_torque(self.hand_name,self.handTorqCoeff[1:]*150,[2,3,4])
-# #        self.bot.set_theta_deg(self.hand_name,[10.0],[0])
-
-#         self.bot.set_mode_theta_gc(self.hand_name,[2,3,4])
-#         self.bot.set_theta_deg(self.hand_name,[self.handCloseLevel*3]*3,[2,3,4])     
-
-#     def step_hold_up1(self):
-#         self.bot.set_mode_theta_gc(self.arm_name)
-
-#         self.bot.set_theta_deg(self.arm_name,self.poses['holdup1'][self.arm_name])
-#         self.bot.set_stiffness(self.arm_name,[self.stiffness/100.0]*self.ndof)
-#         self.bot.set_slew_rate_proportion(self.arm_name,[1.0]*self.ndof)
-#         #self.bot.set_thetadot_deg(self.arm_name,[15.0]*self.ndof)
-
-#     def step_hold_up(self):
-#         self.bot.set_mode_theta_gc(self.arm_name)
-#         self.bot.set_theta_deg(self.arm_name,self.poses['holdup'][self.arm_name])
-#         self.bot.set_stiffness(self.arm_name,[self.stiffness/100.0]*self.ndof)
-#         self.bot.set_slew_rate_proportion(self.arm_name,[1.0]*self.ndof)
-#         #self.bot.set_thetadot_deg(self.arm_name,[15.0]*self.ndof)
-
-       
-#     def step_off(self):
-#         self.bot.set_mode_off(self.arm_name)
-#         self.bot.set_mode_off(self.hand_name)
-
-
-
-# if __name__ == '__main__':
-#     t=M3Proc()
-#     try:
-#         t.start()
-#         t.step_hold_up()
-
-#         t.proxy.step()
-#         t.step_openHand()
-#         t.proxy.step()
-#         # for i in range (30):
-
-#         #     t.step_hold_up1()
-#         #     t.proxy.step()
-#         #     time.sleep(1)
-#         #     t.step_hold_up()
-#         #     t.proxy.step()
-
-#         #     time.sleep(1)
-#         while(1):
-#           t.proxy.step()
-#     except (KeyboardInterrupt,EOFError):
-#         pass
-#     t.stop()
